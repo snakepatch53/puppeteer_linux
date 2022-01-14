6b8370130c6ae7f13126e1f12f39dc519dbc9061
snakepatch53 <snakepatch53@gmail.com>
2022-01-14 09:21:53 -0500

    xd2

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({});
+    const browser = await puppeteer.launch({
+        executablePath: '/usr/bin/chromium-browser'
+    });
     const page = await browser.newPage();
     await page.goto('https://example.com');
     await page.screenshot({
